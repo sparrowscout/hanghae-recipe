782e17690d688532f2bf3c2afdcdd3027b2d5d5b
sparrowscout <105103167+sparrowscout@users.noreply.github.com>
2022-05-10 13:07:48 +0900

    Update app.py
     exposed mongoDB URI 수정

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import certifi
 
 ca = certifi.where()
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.bad8f.mongodb.net/Cluster0?retryWrites=true&w=majority',
+client = MongoClient('...',
                      tlsCAFile=ca)
 db = client.dbsparta
 
